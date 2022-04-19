dd8232a36957e5b865a46655eebc6257fa6719ae
Kapil Bhardwaj (INDIAN) <91521935+Kapil-Bhardwaj@users.noreply.github.com>
2022-04-19 21:41:44 +0530

    Category-Easy

---
+++ Pattern/pattern4.cpp
@@ -0,0 +1,34 @@
+//pattern for printing square of *
+/*   if n = 4;     
+      *
+    * *
+  * * *
+* * * *
+     
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+    cout << "Enter n  : ";
+    cin >> n;
+    for(int i = 1 ; i<= n ; i++)
+    {     
+        for (int k = 1 ; k<=n-i ; k++)
+        {
+            cout<< "   " ;
+        }
+        
+        for(int j = 1 ; j <=i ; j++)
+        {
+            cout << " * ";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
