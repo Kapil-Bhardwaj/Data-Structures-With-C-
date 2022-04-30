7b3d345813f4b33ffb80c7d6bc435623b391012a
Kapil Bhardwaj (INDIAN) <91521935+Kapil-Bhardwaj@users.noreply.github.com>
2022-04-30 21:27:47 +0530

    Creation od=f 2d array

---
+++ Arrays/2dArray/Creation.cpp
@@ -0,0 +1,33 @@
+// 2-D ARRAY
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+    //Creation
+    int arr1[3][3];
+    
+    // Input in Array from user
+     for(int i = 0 ; i<3 ;i++)
+    {
+      for(int j = 0 ; j<3 ; j++)
+         {
+          cin>> arr[i][j] >>" ";
+      }
+         cout<<endl;
+    }
+
+    
+    //Print entire array 
+    for(int i = 0 ; i<3 ;i++)
+    {
+       for(int j = 0 ; j<3 ; j++)
+         {
+           cout<< arr[i][j] <<" ";
+       }
+         cout<<endl;
+    }
+
+    return 0;
+}
